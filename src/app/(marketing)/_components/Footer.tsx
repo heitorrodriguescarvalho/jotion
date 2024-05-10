42521cef87ec1dfbb78884864a8c866b9231599b
Heitor Rodrigues Carvalho <104170136+heitorrodriguescarvalho@users.noreply.github.com>
2024-05-10 14:10:10 +0000

    fix eslint errors

--- src/app/(marketing)/_components/Footer.tsx
+++ src/app/(marketing)/_components/Footer.tsx
@@ -3,9 +3,9 @@ import Logo from './Logo'
 
 export default function Footer() {
   return (
-    <div className="bg-background z-50 flex w-full items-center p-6 dark:bg-[#1f1f1f]">
+    <div className="z-50 flex w-full items-center bg-background p-6 dark:bg-[#1f1f1f]">
       <Logo />
-      <div className="text-muted-foreground flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+      <div className="flex w-full items-center justify-between gap-x-2 text-muted-foreground md:ml-auto md:justify-end">
         <Button variant="ghost" size="sm">
           Privacy Policy
         </Button>
