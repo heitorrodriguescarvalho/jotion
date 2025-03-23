e293b94230b051d1a7ebd405481041d0aadc6f66
Heitor Rodrigues Carvalho <heitorrodrigues2909@gmail.com>
2025-03-22 23:18:01 -0300

    fix: home page light mode background

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import Heroes from './_components/Heroes'
 
 export default function MarketingPage() {
   return (
-    <div className="flex min-h-full flex-col bg-[#1f1f1f]">
+    <div className="flex min-h-full flex-col dark:bg-[#1f1f1f]">
       <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
         <Heading />
         <Heroes />
